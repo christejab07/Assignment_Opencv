d5f811657cfdf9777c1aed368eddfc7d31894b1c
christejab07 <christejab@gmail.com>
2025-02-02 11:19:38 +0200

    decoding barcode

---
+++ decode_barcode.py
@@ -0,0 +1,12 @@
+from pyzbar.pyzbar import decode
+import cv2
+import numpy as np
+
+# Read the image
+image = cv2.imread("IMG_2022.jpg")
+
+# Decode the barcode
+barcodes = decode(image)
+for barcode in barcodes:
+    data = barcode.data.decode("utf-8")
+    print(f"Barcode Data: {data}")
